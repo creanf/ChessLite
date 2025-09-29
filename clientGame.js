5aa5d7d18dff8378dec8a13fdcda9eedf8538bc9
creanf <creanf@rpi.edu>
2025-09-29 14:30:09 +0200

    re-ordering code

--- clientGame.js
+++ clientGame.js
@@ -1,6 +1,10 @@
 const moveDetails = {type: "first", id: null, newId: null, gameStarted: false, yourMove: false, selectionMode: "piece" /**or position */};
 const playerDetails = {isPlayerOne: false, roomId: ""};
 
+// TO DO: Make more functional
+// TO DO: Assign the first player to a random color, then the second to the remaining color
+// TO DO: Close game room at end of game
+
 const getPlayerData = () => {
     const firstPlayerName = localStorage.getItem('firstPlayerName');
     const secondPlayerName = localStorage.getItem('secondPlayerName');
@@ -47,26 +51,16 @@ const getAndEmitPlayerData = () => {
     makeClickEvents();
 }
 
-window.addEventListener('DOMContentLoaded', getAndEmitPlayerData);
-
-socket.on("startGame", (newPlayer) => {
-    console.log("starting game");
-    moveDetails.gameStarted = true;
-    const playerTwoName = newPlayer.playerTwo;
-    document.getElementById("nameSecondPlayer").innerText = "Player 2: " + playerTwoName;
-    if (playerDetails.isPlayerOne){
-        moveDetails.yourMove = true;
-    }
-});
-
-socket.on("move", (moveData) => { //// below stuff coould be condensed into a function
-    console.log("move seen")
+const parseMoveData = (moveData) => {
     const fromId = moveData.from;
     const toId = moveData.to;
     moveDetails.yourMove = true;
     const newPosition = document.getElementById(toId);
     const position = document.getElementById(fromId);
-    //add toId == fromId logic here **
+    return {newPosition, position};
+}
+
+const movePiece = (position, newPosition) => {
     const img = position.querySelector("img");
     const pieceImage = img.getAttribute("src");
     const piece = img.getAttribute("name");
@@ -78,22 +72,7 @@ socket.on("move", (moveData) => { //// below stuff coould be condensed into a fu
     newImg.src = pieceImage;    
     newImg.name = piece;
     newImg.style = style;
-})
-
-document.addEventListener("move", (e) => {
-    const { at } = e.detail;
-    if (moveDetails.type == "first" && moveDetails.gameStarted && moveDetails.yourMove){
-        moveDetails.id = at;
-        moveDetails.type = "second";
-    }
-    else if (moveDetails.type == "second"){
-        moveDetails.newId = at;
-        moveDetails.type = "first";
-        moveDetails.yourMove = false;
-        socket.emit("move", {from: moveDetails.id,to: moveDetails.newId,roomId: playerDetails.roomId});
-        secondClick(moveDetails.newId, moveDetails.id);
-    }
-});
+}
 
 const flipBoard = function() {
     //const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
@@ -130,16 +109,38 @@ const makeClickEvents = function(){
 const secondClick = function(newId, id) {
     const newPosition = document.getElementById(newId);
     const position = document.getElementById(id);
-    ///////////////// FIXME: add newId == id logic here (AKA, do not count it as a move, may need to fix some stuff in the "move" event listener section too)
-    const img = position.querySelector("img");
-    const pieceImage = img.getAttribute("src");
-    const piece = img.getAttribute("name");
-    const style = img.getAttribute("style");
-    img.src = "";
-    img.name = "";
-    img.style = ""
-    const newImg = newPosition.querySelector("img");
-    newImg.src = pieceImage;    
-    newImg.name = piece;
-    newImg.style = style;
+    movePiece(position, newPosition);
 }
+
+window.addEventListener('DOMContentLoaded', getAndEmitPlayerData);
+
+socket.on("startGame", (newPlayer) => {
+    console.log("starting game");
+    moveDetails.gameStarted = true;
+    const playerTwoName = newPlayer.playerTwo;
+    document.getElementById("nameSecondPlayer").innerText = "Player 2: " + playerTwoName;
+    if (playerDetails.isPlayerOne){
+        moveDetails.yourMove = true;
+    }
+});
+
+document.addEventListener("move", (e) => {
+    const { at } = e.detail;
+    if (moveDetails.type == "first" && moveDetails.gameStarted && moveDetails.yourMove){
+        moveDetails.id = at;
+        moveDetails.type = "second";
+    }
+    else if (moveDetails.type == "second"){
+        moveDetails.newId = at;
+        moveDetails.type = "first";
+        moveDetails.yourMove = false;
+        socket.emit("move", {from: moveDetails.id,to: moveDetails.newId,roomId: playerDetails.roomId});
+        secondClick(moveDetails.newId, moveDetails.id);
+    }
+});
+
+socket.on("move", (moveData) => {  // recieved when the other player makes a move
+    console.log("move seen")
+    const {newPosition, position} = parseMoveData(moveData);
+    movePiece(position, newPosition);
+})
