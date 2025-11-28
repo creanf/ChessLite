4805507a0dfdc333b9996094d9d7499d89b2d378
creanf <creanf@rpi.edu>
2025-11-28 22:36:11 +0100

    move logic aside from checks, checkmate, and queening functional

--- clientGame.js
+++ clientGame.js
@@ -1,6 +1,8 @@
-const moveDetails = {type: "first", id: null, newId: null, gameStarted: false, yourMove: false, pieceBoardCoords: null, pieceName: "", takenPiece: "", selectionMode: "piece"};
+const moveDetails = {type: "first", id: null, newId: null, gameStarted: false, yourMove: false, pieceBoardCoords: null, pieceName: "", selectionMode: "piece"};
 const playerDetails = {isPlayerOne: false, roomId: ""};
 const takenPieces = {};
+// FIXME: board and boardGrid variable names are confusing
+// FIXME: player names not swapping - make this a part of the "flipBoard" function
 const board = [ ["wR","wN","wB","wQ","wK","wB","wN","wR"],["wP","wP","wP","wP","wP","wP","wP","wP"],["__","__","__","__","__","__","__","__"],["__","__","__","__","__","__","__","__"],["__","__","__","__","__","__","__","__"],["__","__","__","__","__","__","__","__"],["bP","bP","bP","bP","bP","bP","bP","bP"],["bR","bN","bB","bQ","bK","bB","bN","bR"] ]; // the html selection stuff is unnecessary if this works
 const letterConvert = {"a":0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7};
 
@@ -8,6 +10,7 @@ const letterConvert = {"a":0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7};
 // TO DO: Close game room at end of game
 // TO DO: Use moveOnArray and isValidMove in main move event listener, print the board to check if working
 // TO DO: Queening
+// TO DO: Fix vscode config
 
 // TO DO NEXT: Check if the pawn move function works
 
@@ -103,6 +106,8 @@ const movePieceOnArray = (id, newId) => { //TO DO
     const currPiece = board[currPos.row][currPos.col];
     const destPiece = board[destPos.row][destPos.col];
 
+    console.log("dest piece - " + destPiece);
+
     // add to taken pieces here
 
     board[currPos.row][currPos.col] = "__";
@@ -111,37 +116,14 @@ const movePieceOnArray = (id, newId) => { //TO DO
     printBoard();
 }
 
-// @returns bool which is true if the move is value
-const isValidMove = (moveDetails) => { //TO DO
-    const {pieceName} = moveDetails; // these give piece coordinates, not the actual piece
-    console.log(pieceName);
-    if (playerDetails.isPlayerOne && pieceName[0] == "b") {
-        return false;
-    }
-    if (!playerDetails.isPlayerOne && pieceName[0] == "w") {
-        return false;
-    }
-    if (pieceName[1] == "P" && checkPawnMove(moveDetails)){
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
 const flipBoard = function() {
-    //const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-    // first copy all existing elements into an so we have copies to reference
     const boardGrid = document.getElementById("boardGrid");
     const squaresArray = Array.from(boardGrid.querySelectorAll("div")); // (static, this won't be updated in the for loop, so we can treat it as a copy)
     boardGrid.innerHTML = "";
-    // then add the new elements in order
     for (let i = 7; i > -1; i--){
         for (let j = 7; j > -1 ; j--){
-            //const currLetter = letters[j];
             const arrayNumber = i * 8 + j;
             const piece = squaresArray[arrayNumber]
-            //piece.id = String(currLetter + i);
             boardGrid.appendChild(piece);
         }
     }
@@ -209,6 +191,66 @@ socket.on("move", (moveData) => {  // recieved when the other player makes a mov
     movePiece(position, newPosition);
 })
 
+// @returns bool which is true if the move is value
+const isValidMove = (moveDetails) => { //TO DO
+    const {pieceName} = moveDetails; // these give piece coordinates, not the actual piece
+    console.log(pieceName);
+    if (playerDetails.isPlayerOne && pieceName[0] == "b") {
+        return false;
+    }
+    if (!playerDetails.isPlayerOne && pieceName[0] == "w") {
+        return false;
+    }
+    if (pieceName[0] == "w"){
+        if (pieceName[1] == "P" && checkPawnMove(moveDetails)){ // add curr color and oppcolor parameters
+            return true;
+        }
+        else if (pieceName[1] == "R" && checkRookMove(moveDetails, "w", "b")){
+            return true;
+        }
+        else if (pieceName[1] == "N" && checkKnightMove(moveDetails, "w", "b")){
+            return true;
+        }
+        else if (pieceName[1] == "B" && checkBishopMove(moveDetails, "w", "b")){
+            return true;
+        }
+        else if (pieceName[1] == "K" && checkKingMove(moveDetails, "w", "b")){
+            return true;
+        }
+        else if (pieceName[1] == "Q" && checkQueenMove(moveDetails, "w", "b")){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    if (pieceName[0] == "b"){
+        if (pieceName[1] == "P" && checkPawnMove(moveDetails)){ // add curr color and oppcolor parameters
+            return true;
+        }
+        else if (pieceName[1] == "R" && checkRookMove(moveDetails, "b", "w")){
+            return true;
+        }
+        else if (pieceName[1] == "N" && checkKnightMove(moveDetails, "b", "w")){
+            return true;
+        }
+        else if (pieceName[1] == "B" && checkBishopMove(moveDetails, "b", "w")){
+            return true;
+        }
+        else if (pieceName[1] == "K" && checkKingMove(moveDetails, "b", "w")){
+            return true;
+        }
+        else if (pieceName[1] == "Q" && checkQueenMove(moveDetails, "b", "w")){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+// TO DO: Refactor with convertToRowCol and with the new color system to reduce redundancy
+// currColor and oppColor can either be "w" or "b"
 const checkPawnMove = (moveData) => {
     const {id, newId, pieceName} = moveData;
     const currRow = id[1]-1;
@@ -292,47 +334,209 @@ const checkPawnMove = (moveData) => {
     }
 }
 
-const checkRookMove = (moveData) => {
-    if (moveData.piece[0] == "w") {
+// this function is used to check if any pieces are "in the way" for 8 possible directions (up, down, left, right, diagonals) - the direction is given by inputting a vertical iterator and a horizontal iterator (i.e. 1, 1 for checking the northwest direction)
+// this is used in the checkBishopMove, checkRookMove, and checkQueenMove functions
+// returns true iff there is a piece "in the way"
+// a piece being at "newPos" or "currPos" will not cause this function to return true
+// assumes that newPos will eventually be reached
+const inTheWay = (currPos, newPos, vertItr, horiItr) => {
+    const checkPos = {row: currPos.row + vertItr, col: currPos.col + horiItr};
+    while (checkPos.row != newPos.row || checkPos.col != newPos.col) {
+        console.log("checking" + checkPos.row + " " + checkPos.col)
+        const checkPieceName = board[checkPos.row][checkPos.col];
+        if (checkPieceName != "__") {
+            return true;
+        }
+        checkPos.row += vertItr;
+        checkPos.col += horiItr;
+    }
+    return false;
+}
 
+const checkRookMove = (moveData, currColor, oppColor) => {
+    const {id, newId, pieceName} = moveData;
+    const currPos = convertToRowCol(id);
+    const newPos = convertToRowCol(newId);
+    const targetName = board[newPos.row][newPos.col];
+    // check of the new position is the same as the old position or if the piece was not moved in a straight line
+    if (newPos.row == currPos.row && newPos.col == currPos.col || newPos.row != currPos.row && newPos.col != currPos.col) {
+        return false;
+    }
+    // check if a piece got in the way of the move
+    if (newPos.row > currPos.row) {
+        if (inTheWay(currPos, newPos, 1, 0)){
+            return false;
+        }
+    }
+    else if (newPos.row < currPos.row) {
+        if (inTheWay(currPos, newPos, -1, 0)){
+            return false;
+        }
+    }
+    else if (newPos.col > currPos.col) {
+        if (inTheWay(currPos, newPos, 0, 1)){
+            return false;
+        }
+    }
+    else { // newPos.col < currPos.col
+        if (inTheWay(currPos, newPos, 0, -1)){
+            return false;
+        }
+    }
+    // handles the case where a piece is taken
+    if (targetName != "__"){
+        if (targetName[0] != oppColor) {
+            return false;
+        }
+        else {
+            // add to taken pieces FIXME
+            return true;
+        }
     }
     else {
-        
+        return true;
     }
 }
 
-const checkKnightMove = (moveData) => {
-    if (moveData.piece[0] == "w") {
-
+const checkKnightMove = (moveData, currColor, oppColor) => {
+    const {id, newId, pieceName} = moveData;
+    const currPos = convertToRowCol(id);
+    const newPos = convertToRowCol(newId);
+    const targetName = board[newPos.row][newPos.col];
+    // isValid variable used instead of returning so we can check if a piece was targeted later
+    let isValid = false;
+    // knight can only move to 1 of 8 positions
+    if (newPos.row == currPos.row + 2 && newPos.col == currPos.col - 1) {
+        isValid = true;
+    }
+    else if (newPos.row == currPos.row + 2 && newPos.col == currPos.col + 1) {
+        isValid = true;
+    }
+    else if (newPos.row == currPos.row - 2 && newPos.col == currPos.col - 1) {
+        isValid = true;
+    }
+    else if (newPos.row == currPos.row - 2 && newPos.col == currPos.col + 1) {
+        isValid = true;
+    }
+    else if (newPos.row == currPos.row + 1 && newPos.col == currPos.col - 2) {
+        isValid = true;
+    }
+    else if (newPos.row == currPos.row + 1 && newPos.col == currPos.col + 2) {
+        isValid = true;
+    }
+    else if (newPos.row == currPos.row - 1 && newPos.col == currPos.col - 2) {
+        isValid = true;
+    }
+    else if (newPos.row == currPos.row - 1 && newPos.col == currPos.col + 2) {
+        isValid = true;
     }
     else {
-        
+        isValid = false;
+    }
+    if (isValid) {
+        if (targetName != "__") {
+            if (targetName[0] != oppColor){
+                // add to taken pieces
+                return false;
+            }
+            return true;
+        }
+        else {
+            return true;
+        }
+    }
+    else {
+        return false;
     }
 }
 
-const checkBishopMove = (moveData) => {
-    if (moveData.piece[0] == "w") {
-
+const checkBishopMove = (moveData, currColor, oppColor) => {
+    const {id, newId, pieceName} = moveData;
+    const currPos = convertToRowCol(id);
+    const newPos = convertToRowCol(newId);
+    const targetName = board[newPos.row][newPos.col];
+    // check each of the four directions first
+    if (currPos.row < newPos.row && currPos.col < newPos.col) {
+        if (inTheWay(currPos, newPos, 1, 1)) {
+            return false;
+        }
+    }
+    else if (currPos.row < newPos.row && currPos.col > newPos.col) {
+        if (inTheWay(currPos, newPos, 1, -1)) {
+            return false;
+        }
+    }
+    else if (currPos.row > newPos.row && currPos.col > newPos.col) {
+        if (inTheWay(currPos, newPos, -1, -1)) {
+            return false;
+        }
+    }
+    else if (currPos.row > newPos.row && currPos.col < newPos.col) {
+        if (inTheWay(currPos, newPos, -1, 1)) {
+            return false;
+        }
     }
     else {
-        
+        return false;
     }
+    if (targetName != "__") {
+        if (targetName[0] != oppColor){
+            return false;
+        }
+        // handle capture
+        return true;
+    }
+    return true;
 }
 
-const checkKingMove = (moveData) => {
-    if (moveData.piece[0] == "w") {
-
+const checkKingMove = (moveData, currColor, oppColor) => {
+    const {id, newId, pieceName} = moveData;
+    const currPos = convertToRowCol(id);
+    const newPos = convertToRowCol(newId);
+    const targetName = board[newPos.row][newPos.col];
+    let isValid = false;
+    // simply check all eight directions
+    if (newPos.row == currPos.row + 1 && newPos.col == currPos.col + 1) {
+        isValid = true;
+    }
+    else if (newPos.row == currPos.row - 1 && newPos.col == currPos.col + 1) {
+        isValid = true;
+    }
+    else if (newPos.row == currPos.row + 1 && newPos.col == currPos.col - 1) {
+        isValid = true;
+    }
+    else if (newPos.row == currPos.row - 1 && newPos.col == currPos.col - 1) {
+        isValid = true;
+    }
+    else if (newPos.row == currPos.row + 0 && newPos.col == currPos.col + 1) {
+        isValid = true;
+    }
+    else if (newPos.row == currPos.row + 0 && newPos.col == currPos.col - 1) {
+        isValid = true;
+    }
+    else if (newPos.row == currPos.row + 1 && newPos.col == currPos.col + 0) {
+        isValid = true;
+    }
+    else if (newPos.row == currPos.row - 1 && newPos.col == currPos.col + 0) {
+        isValid = true;
     }
     else {
-        
+        return false;
     }
+    if (isValid)  {// redundant
+        if (targetName != "__"){
+            if (targetName[0] != oppColor){
+                return false;
+            }
+            // handleCapture
+            return true;
+        }
+        return true;
+    }
+    return true;
 }
 
-const checkQueenMove = (moveData) => {
-    if (moveData.piece[0] == "w") {
-
-    }
-    else {
-        
-    }
+const checkQueenMove = (moveData, currColor, oppColor) => {
+    // can just use checkRook and checkBishop
+    return checkBishopMove(moveData, currColor, oppColor) || checkRookMove(moveData, currColor, oppColor);
 }
